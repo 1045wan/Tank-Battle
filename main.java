cc9afb3ebab9db25cd599657f5a939de9741ba4a
liu-iuronglxr <106054345+liu-iuronglxr@users.noreply.github.com>
2022-05-23 20:45:54 +0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,17 @@
+package cmo.lxr.tank;
+
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class main {
+
+	public static void main(String[] args) {
+	
+		tankFrame tl=new tankFrame();
+		
+			
+
+	}
+
+}
