383cf8e78ece3c073f65fb3d1eaa6410ef7d5f26
liuyonghong2020402058 <106074927+liuyonghong2020402058@users.noreply.github.com>
2022-05-24 20:56:44 +0800

    Add files via upload

--- TankFrame.java
+++ TankFrame.java
@@ -1,21 +1,27 @@
 package com.liuyonghong.tank;
 
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class TankFrame extends Frame {
 	
-	int x = 200,y =200;
-	Dir dir = Dir.DOWN;
-	private static final int SPEED =10;
+	
+
+	Tank myTank = new Tank(200,200,Dir.DOWN,this);
+	List<Bullet> bullets =new ArrayList<>();
+	static final int GAME_WIDTH = 800,GAME_HEIGHT = 600;
 	
     public TankFrame() {
-   setSize(800,600);
+    setSize(800,600);
     setResizable(false);
     setTitle("tank war");
     setVisible(true);
@@ -30,27 +36,42 @@ public class TankFrame extends Frame {
     	}
     });
   }
+   //消除闪烁
+    Image offScreenImage = null;
+    @Override
+    public void update(Graphics g) {
+    	if(offScreenImage == null) {
+    		offScreenImage = this.createImage(GAME_WIDTH,GAME_HEIGHT);
+    	}
+    	Graphics gOffScreen =offScreenImage.getGraphics();
+    	Color c = gOffScreen.getColor();
+    	gOffScreen.setColor(Color.PINK);
+    	gOffScreen.fillRect(0, 0,GAME_WIDTH ,GAME_HEIGHT);
+    	gOffScreen.setColor(c);
+    	paint(gOffScreen);
+    	g.drawImage(offScreenImage, 0, 0, null);
+    }
+    
     
     @Override
    public void paint(Graphics g) {
-	 g.fillRect(x, y, 50, 50);
-	 
-	 switch(dir) {
-	 case LEFT:
-		 x-=SPEED;
-		 break;
-	 case UP:
-		 y-=SPEED;
-		 break;
-	 case RIGHT:
-		 x+=SPEED;
-		 break;
-	 case DOWN:
-		 y+=SPEED;
-		 break;
-	 }
-	// x +=10;
-	// y +=10;
+    	Color c = g.getColor();
+    	g.setColor(Color.WHITE);
+    	g.drawString("子弹的数量:"+ bullets.size(),10,60);
+    	g.setColor(c);
+    //消除子弹列表的内存泄露，处理迭代器中的删除问题	
+    //方法一	
+    	myTank.paint(g);
+    	for(int i=0; i<bullets.size();i++) {
+    		bullets.get(i).paint(g);
+  	}
+	//方法二
+    //for(Iterator<Bullet>it = bullets.iterator();it.hasNext();) {
+	//	Bullet b = it.next();
+	//	if(!b.live)it.remove();	}
+    	
+    
+    	
    }
     
     //记录按键状态来控制坦克方向
@@ -77,7 +98,13 @@ public class TankFrame extends Frame {
             case KeyEvent.VK_DOWN:
             	bD = true;
 	            break;
-				
+			
+            case KeyEvent.VK_CONTROL:
+            myTank.fire();    
+            break;
+            
+            
+            
 				default:
 					break;
 			}
@@ -100,6 +127,10 @@ public class TankFrame extends Frame {
             case KeyEvent.VK_DOWN:
             	bD = false;
 	            break;
+	            
+            case KeyEvent.VK_CONTROL:
+            	myTank.fire();
+            	break;
 				
 				default:
 					break;
@@ -108,12 +139,23 @@ public class TankFrame extends Frame {
 		}
     	
 		private void setMainTankDir() {
-			if(bL) dir = Dir.LEFT;
-			if(bU) dir = Dir.UP;
-			if(bR) dir = Dir.RIGHT;
-			if(bD) dir = Dir.DOWN;
+			
+			if(!bL && !bU && !bR && !bD) myTank.setMoving(false); //添加坦克禁止的处理，只要四个键都没有按下，坦克就是静止的
+			else {
+			myTank.setMoving(true);
+			
+			if(bL) myTank.setDir(Dir.LEFT);
+			if(bU) myTank.setDir(Dir.UP);
+			if(bR) myTank.setDir(Dir.RIGHT);
+			if(bD) myTank.setDir(Dir.DOWN);
+			
+	       }	
+		
 			
 		}
+   
     }
-   }
+  
+}
+
    
